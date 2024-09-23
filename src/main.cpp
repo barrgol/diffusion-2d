8b42f1706065995cd4e3d19ae91020b745e88061
wykremcony <bartek.golik@student.uva.nl>
2024-09-23 21:25:21 +0200

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        sf::CircleShape c{ 100.0f };
+        c.setFillColor(sf::Color::Green);
+        window.draw(c);
+
+        window.display();
+    }
+}
